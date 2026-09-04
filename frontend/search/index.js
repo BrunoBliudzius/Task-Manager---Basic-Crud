d2df139af46b5c09c2f68d53f77cf0a9c3755374
Bruno <brunosilvabliu@gmail.com>
2026-09-04 18:15:37 -0300

    fix links

--- frontend/search/index.js
+++ frontend/search/index.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 async function loadAppointments() {
   try {
-    const response = await fetch("http://127.0.0.1:8000/appointments/");
+    const response = await fetch("http://127.0.https://task-manager-basic-crud-production.up.railway.app/appointments/.1:8000/appointments/");
 
     const data = await response.json();
 
