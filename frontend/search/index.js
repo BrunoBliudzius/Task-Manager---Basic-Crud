81579421e664f8f2f5b2a4777dca3165250727c7
Bruno <brunosilvabliu@gmail.com>
2026-09-10 19:02:04 -0300

    front end connecting api

--- frontend/search/index.js
+++ frontend/search/index.js
@@ -12,21 +12,59 @@ async function loadAppointments() {
 
     const tbody = document.getElementById("appointments-table");
 
+    tbody.innerHTML = "";
+
     data.forEach((appointment) => {
       const row = document.createElement("tr");
-      row.innerHTML = `<td>${appointment.id}</td>
-                    <td>${appointment.title}</td>
-                    <td>${appointment.description}</td>
-                    <td>${appointment.date}</td>
-                    <td>${appointment.end_time}</td>
-                    <td>
-                    <a class="btn btn-sm btn-primary" href="#"> Update </a>
-                    <a class="btn btn-sm btn-danger" href="#"> Delete </a>
-                    </td> `;
+      row.innerHTML = `
+        <td>${appointment[0]}</td>
+        <td>${appointment[1]}</td>
+        <td>${appointment[2]}</td>
+        <td>${appointment[3]}</td>
+        <td>${appointment[4]}</td>
+
+
+        <td>
+            <button
+                class="btn btn-warning"
+                data-bs-toggle="modal"
+                data-bs-target="#appointmentModal"
+                data-id="${appointment.id}"
+            >
+                update
+            </button>
+
+            <button
+                class="btn btn-danger delete-button"
+                onclick="deleteAppointment(${appointment[0]})"
+            >
+                Delete
+            </button>
+        </td>
+      `;
 
       tbody.appendChild(row);
     });
   } catch (error) {
     console.error("Erro:", error);
+  }
+}
+
+async function deleteAppointment(id) {
+  try {
+    const response = await fetch(
+      `https://task-manager-basic-crud-production.up.railway.app/api/v1/appointments/${id}`,
+      {
+        method: "DELETE",
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+
+    loadAppointments();
+  } catch (error) {
+    console.error("Error:", error);
   }
 }
